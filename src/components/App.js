c29fe7d810a3f3ed018590a08b5cc1e8352051c0
li.li <li.li@liulishuo.com>
2017-11-17 17:59:26 +0800

    :beers: just test

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render () {
     return (
       <div className="app">
-        <h1>Hello React</h1>
+        <h1>Hello React, Test Netlify CD</h1>
       </div>
     )
   }
